6e3524a218a918761fa9eaa04fc4a75368d4cba4
Sidiqjon <sidiqjonyusufjanov7@gmail.com>
2025-05-11 22:00:17 +0500

    Completed

--- src/components/product-crud/ProductCrud.jsx
+++ src/components/product-crud/ProductCrud.jsx
@@ -91,7 +91,7 @@ const ProductCrud = () => {
       </div>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-        {products.map((product) => (
+        {products?.map((product) => (
           <ProductCard
             key={product.id}
             product={product}
